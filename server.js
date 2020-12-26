2c7adfce21f2f09ef234aaf4ed2eb6e0daf21316
Danny Maier <dannymaier@gmail.com>
2020-12-25 16:16:47 -0800

    added starter files; git ignore, hidden resources, installed dependencies, starter server and schema files

---
+++ server.js
@@ -0,0 +1,7 @@
+//DEPENDENCIES
+// Import the mysql package
+const mysql = require('mysql');
+// Import the inquirer package
+const inquirer = require("inquirer");
+// Import console.table
+const cTable = require('console.table');
