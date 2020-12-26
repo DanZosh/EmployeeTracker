5f37b78066d3f1db757164240b8d6b3d109e02b5
Danny Maier <dannymaier@gmail.com>
2020-12-26 15:56:33 -0800

    i got the tables to show for department, employee and roles with the seeded data. easy part done

--- index.js
+++ index.js
@@ -12,55 +12,73 @@
 	//Import art	
 	const art = require("./art")
 	//Import db folder	
-	const db = require("./db")
+	const db = require("./db");
+const connection = require('./db/connection');
 
-db.getDepartments().then((results) =>
-	console.table(results)
-)
 
-// function startQuestions(){
 
-// 	const startMenu = [
-// 		{
-// 			type: 'list',
-// 			name: 'startMenu',
-// 			message: 'WHAT WOULD YOU LIKE TO DO?',
-// 			choices:[
-// 				'1. View all employees',
-// 				'2. View all employees by department',
-// 				'3. View all employees by manager',
-// 				'4. Add employee',
-// 				'5. Remove employee',
-// 				'6. Update employee role',
-// 				'7. Update employee manager',
-// 				'8. View all roles',
-// 				'9. Add roles',
-// 				'10. Remove roles',
-// 				'11. View all departments',
-// 				'12. Add department',
-// 				'13. Remove department',
-// 				'14. QUIT'
-// 			]
-// 		}];
-// 	inquirer
-// 		.prompt(startMenu)
-// 		.then((response) => {
-// 			// console.log('success');
-// 			console.log(response);
-// 			console.log(startMenu[0]);
-// 			switch(response){
-// 				case startMenu[0]:
-// 					// function question1(){
-// 						console.log("here are the employees");
-// 						break;
+function startQuestions(){
+
+	const startMenu = [
+		{
+			type: 'list',
+			name: 'startMenu',
+			message: 'WHAT WOULD YOU LIKE TO DO?',
+			choices:[
+				'1. View all employees',
+				// '2. View all employees by department',
+				// '3. View all employees by manager',
+				// '4. Add employee',
+				// '5. Remove employee',
+				// '6. Update employee role',
+				// '7. Update employee manager',
+				'8. View all roles',
+				// '9. Add roles',
+				// '10. Remove roles',
+				'11. View all departments',
+				// '12. Add department',
+				// '13. Remove department',
+				'14. QUIT \n \n'
+			]
+		}];
+	inquirer
+		.prompt(startMenu)
+		.then((response) => {
+			// console.log('success');
+			// console.log(response.startMenu);
+			switch(response.startMenu){
+				case '1. View all employees':
+					console.log("Here are the employees:");
+					db.getEmployees().then((results) =>
+					console.table(results)
+					)
+					startQuestions()
+					break;
 				
-// 				case startMenu[1]:
-// 					// question2(){
-// 						console.log("here are the departments");
-// 						break;
-// 					}
-// 			}
-// 		);	
-// }
+				case '8. View all roles':
+					console.log("Here are the roles:");
+					db.getRoles().then((results) =>
+					console.table(results)
+					)
+					startQuestions()
+					break;
 
-// startQuestions()
+				case '11. View all departments':
+					console.log("Here are the departments:");
+					db.getDepartments().then((results) =>{
+					console.table(results);
+					console.log("\n");
+					})
+					// .then(startQuestions())
+					// startQuestions()
+					break;
+				
+				default:
+					connection.end();
+
+					}
+			}
+		);	
+}
+
+startQuestions()
