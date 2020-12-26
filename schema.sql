5370b550da6dd39a59d7fcd4ccec0bd6cd61eb67
Danny Maier <dannymaier@gmail.com>
2020-12-25 17:32:27 -0800

    got my boilerplate code into the server file, organized my schema and seeded the tables in mysql workbench. the connection is working.

---
+++ schema.sql
@@ -0,0 +1,64 @@
+-- Drops the employees_db if it exists currently --
+DROP DATABASE IF EXISTS employees_db;
+-- Creates the "employees_db" database --
+CREATE DATABASE employees_db;
+-- Makes it so all of the following code will affect employees_db --
+USE employees_db;
+
+-- department TABLE
+-- Creates the table "people" within employees_db --
+CREATE TABLE department (
+-- Creates a numeric column called "id" which will automatically increment its default value as we create new rows --
+id INT(11) AUTO_INCREMENT NOT NULL,
+-- Makes a string column called "name" which cannot contain null --
+name VARCHAR(30) NOT NULL,
+-- Sets id as this table's primary key which means all data contained within it will be unique --
+PRIMARY KEY (id)
+);
+-- Creates new rows containing data in all named columns --
+INSERT INTO department (name)
+VALUES ("Sales");
+
+SELECT * FROM department;
+
+-- role TABLE
+-- Creates the table "role" within employees_db --
+CREATE TABLE role (
+-- Creates a numeric column called "id" which will automatically increment its default value as we create new rows --
+id INT(11) AUTO_INCREMENT NOT NULL,
+-- Makes a string column called "title" which cannot contain null --
+title VARCHAR(30) NOT NULL,
+-- Makes an integer column called "salary" which cannot contain null --
+salary DECIMAL(10,2) NOT NULL,
+-- Makes an integer column called "department_id" which cannot contain null --
+department_id INT(11) NOT NULL,
+-- Sets id as this table's primary key which means all data contained within it will be unique --
+PRIMARY KEY (id)
+);
+-- Creates new rows containing data in all named columns --
+INSERT INTO role (title, salary, department_id)
+VALUES ("Manager", 100000, 1);
+
+SELECT * FROM role;
+
+-- role TABLE
+-- Creates the table "employee" within employees_db --
+CREATE TABLE employee (
+-- Creates a numeric column called "id" which will automatically increment its default value as we create new rows --
+id INT(11) AUTO_INCREMENT NOT NULL,
+-- Makes a string column called "first_name" which cannot contain null --
+first_name VARCHAR(30) NOT NULL,
+-- Makes a string column called "last_name" which cannot contain null --
+last_name VARCHAR(30) NOT NULL,
+-- Makes an integer column called "role_id" which cannot contain null --
+role_id INT(11) NOT NULL,
+-- Makes an integer column called "manager_id" which cannot contain null --
+manager_id INT(11),
+-- Sets id as this table's primary key which means all data contained within it will be unique --
+PRIMARY KEY (id)
+);
+-- Creates new rows containing data in all named columns --
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Dan", "Maier", 2, 4);
+
+SELECT * FROM employee;
--- server.js
+++ server.js
@@ -8,3 +8,19 @@ const cTable = require('console.table');
 //Import art
 const art = require("./art")
     /* and now run */ art()
+
+
+	// Create the connection object
+	const connection = mysql.createConnection({
+	host: 'localhost',
+	port: 3306,
+	user: 'root',
+	password: '6Japan9!',
+	database: "employees_db",
+	});
+	// Run the connection
+	connection.connect((err) => {
+	if (err) throw err;
+	console.log('connected as id ' + connection.threadId);
+	connection.end();
+	});
